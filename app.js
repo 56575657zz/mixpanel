ac4e20b31d80c83442f4f65ce8f8a970ac5a08d8
56575657zz <56575657zz@gmail.com>
2026-06-03 05:30:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const app = express();
+const Mixpanel = require('mixpanel');
 
-const campaignRedirect = require('./analytics/campaign-redirect');
-app.use('/go', campaignRedirect);
+const app = express();
+const mixpanel = Mixpanel.init(
+  process.env.MIXPANEL_TOKEN || '47fac8a25bc9bd4269790f2210198f54'
+);
+
+app.get('/go', (req, res) => {
+  mixpanel.track('$campaign_link_click', {
+    distinct_id: req.ip,
+    url: 'https://lakhianilaws.com/?vdlztpxt',
+    $insert_id: `click-${Date.now()}`,
+  });
+  res.redirect(302, 'https://lakhianilaws.com/?vdlztpxt');
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
